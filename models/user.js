047a6fa07f84faf7b2f95a73df9ac167b40b8d30
Varun Bale <varun.bale@gmail.com>
2020-06-07 12:08:04 +0530

    Mongoose population

--- models/user.js
+++ models/user.js
@@ -3,13 +3,21 @@ var Schema = mongoose.Schema;
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
+    firstname: {
+      type: String,
+        default: ''
+    },
+    lastname: {
+      type: String,
+        default: ''
+    },
     admin:   {
         type: Boolean,
         default: false
     }
 });
 
-User.plugin(passportLocalMongoose);
 
+User.plugin(passportLocalMongoose);
 
 module.exports = mongoose.model('User', User);
