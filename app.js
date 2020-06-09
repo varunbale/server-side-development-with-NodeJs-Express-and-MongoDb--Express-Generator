144125bbde124689e2f9a5b4eba0b061e03dd9e3
Varun Bale <varun.bale@gmail.com>
2020-06-09 17:44:44 +0530

    Assignmnet-4

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ var dishRouter=require('./routes/dishRouter');
 var promoRouter=require('./routes/promoRouter');
 var leaderRouter=require('./routes/leaderRouter');
 var uploadRouter=require('./routes/uploadRouter');
+var favouriteRouter = require('./routes/favoriteRouter');
 
 const mongoose = require('mongoose');
 
@@ -63,6 +64,7 @@ app.use('/dishes',dishRouter);
 app.use('/leaders',leaderRouter);
 app.use('/promotions',promoRouter);
 app.use('/imageUpload',uploadRouter);
+app.use('/favorites',favouriteRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
